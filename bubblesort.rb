e7393d201812fec767d8ae61175ad0b31377e72f
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-18 14:17:14 -0800

    added comments

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,10 +1,10 @@
 array = [4,3,78,2,0,2]
 
 def bubble_sort(array)
-  for g in 0..(array.length-1)
-    for i in 0..(array.length-2)
+  for g in 0..(array.length-1) ##max number of sorts is length - 1
+    for i in 0..(array.length-2) ##end at the second last number so you can do i+1
       if array[i] > array[i+1]
-        tempNum = array[i]
+        tempNum = array[i]     ##init a temp number to hold while you switch them around
         array[i] = array[i+1]
         array[i+1] = tempNum
       end
