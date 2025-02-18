d036c35e99660c7e21f5672e47478369ef1f8bf2
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-17 19:26:36 -0800

    added logic to wrap z -> a. finished project

--- cipher.rb
+++ cipher.rb
@@ -6,9 +6,17 @@ def caesar_cipher(input_string, shift)
 
   new_string = string_array.map do |letter|
     if uppercase_alphabet.include?(letter) 
-      letter = uppercase_alphabet[uppercase_alphabet.index(letter)+shift]
+      if uppercase_alphabet.index(letter)+shift > 26
+        letter = uppercase_alphabet[uppercase_alphabet.index(letter)+shift-26]
+      else
+        letter = uppercase_alphabet[uppercase_alphabet.index(letter)+shift]
+      end
     elsif lowercase_alphabet.include?(letter)
-      letter = lowercase_alphabet[lowercase_alphabet.index(letter)+shift]
+      if lowercase_alphabet.index(letter)+shift > 26
+        letter = lowercase_alphabet[lowercase_alphabet.index(letter)+shift-26]
+      else 
+        letter = lowercase_alphabet[lowercase_alphabet.index(letter)+shift]
+      end
     end
     letter
   end
